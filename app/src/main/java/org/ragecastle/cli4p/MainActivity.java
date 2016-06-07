0e0646de6d109d73abde68177fdd76c35cb80c97
James Hall <gymboh32@gmail.com>
2016-06-06 21:20:12 -0400

    Day one - Basic layout

--- app/src/main/java/org/ragecastle/cli4p/MainActivity.java
+++ app/src/main/java/org/ragecastle/cli4p/MainActivity.java
@@ -2,12 +2,20 @@ package org.ragecastle.cli4p;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v){
+    // Send message
     }
 }
